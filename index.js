8c004bb78c052a7321b96d0aca581e9b451026f0
离央 <miaoyou.gmy@alibaba-inc.com>
2017-03-05 17:50:06 +0800

    set workers=1

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 'use strict';
 
-// npm run dev DO NOT read this file
-
 require('egg').startCluster({
   baseDir: __dirname,
   port: process.env.PORT || 7001, // default to 7001
+  workers:1,
 });
