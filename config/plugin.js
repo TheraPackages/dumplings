ac040f810b78c46c4ad14acbd947f2ce0d357375
离央 <miaoyou.gmy@alibaba-inc.com>
2017-03-06 21:59:53 +0800

    删除无用配置

--- config/plugin.js
+++ config/plugin.js
@@ -1,17 +1,6 @@
 'use strict';
-
-// had enabled by egg
-// exports.static = true;
-
 exports.nunjucks = {
 	enable: true,
 	package: 'egg-view-nunjucks'
 };
 
-module.exports = {
-  security: {
-    csrf: {
-      enable: false,
-    },
-  },
-};
