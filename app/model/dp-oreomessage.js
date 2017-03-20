a011a36eb96c92f11029a6999311394cbd972f30
778477 <1032419360@qq.com>
2017-03-20 11:21:12 +0800

    close #1

--- app/model/dp-oreomessage.js
+++ app/model/dp-oreomessage.js
@@ -34,8 +34,16 @@ function filterLogs(logs, logLevel) {
         return curLevel <= specifyLevel
     })
 }
-
-let createOreoMessage = function (type, template, logs, name, bundleUrl) {
+/**
+ * 
+ * @param string type     监听文件对应的渲染方式，目前仅支持 weex
+ * @param string template 文件转化之后的js模板内容
+ * @param array logs      文件转化产生的日志
+ * @param string name     转出文件名(.js)
+ * @param string bundleUrl 转出文件路径(/xx/x.js)
+ * @param string fileName  原文件名(.we/.vue)
+ */
+let createOreoMessage = function (type, template, logs, name, bundleUrl,fileName) {
     const model = {
         'message': message,
         'data': {
@@ -45,7 +53,8 @@ let createOreoMessage = function (type, template, logs, name, bundleUrl) {
                     'bundleUrl': bundleUrl,
                     'type': type,
                     'template': template,
-                    'logs': filterLogs(logs, 'ERROR')
+                    'logs': filterLogs(logs, 'ERROR'),
+                    'fileName':fileName
                 }
             ],
         }
