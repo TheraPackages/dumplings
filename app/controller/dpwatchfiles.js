0aa3ee23948c24b229f043375199ee49861eca19
778477 <1032419360@qq.com>
2017-03-27 17:13:21 +0800

    remove comments

--- app/controller/dpwatchfiles.js
+++ app/controller/dpwatchfiles.js
@@ -10,15 +10,5 @@ module.exports = function * () {
         title: 'watchFiles',
     };
 
-    // if (this.req.method === 'POST') {
-    //     if (this.request.body.cmd === 'run') {
-    //         this.app.gazeWather.add(this.request.body.file);
-    //         this.app.watchFilesMap.set(this.request.body.file, this.request.body.renderProtocol);
-    //     } else if (this.request.body.cmd === 'stop') {
-    //         this.app.gazeWather.remove(this.request.body.file);
-    //         this.app.watchFilesMap.delete(this.request.body.file);
-    //     }
-    // }
-
     this.response.body = JSON.stringify(this.app.gazeWather.watched());
 };
